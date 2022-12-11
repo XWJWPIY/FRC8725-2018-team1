493bcaf3489002f3970acf2943048b5ec984ccdd
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2022-12-11 23:39:46 +0800

    新增手動模式 Command 的程式(尚未完全註解完畢)

--- Teamwork_1/src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ Teamwork_1/src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -3,7 +3,8 @@ package frc.robot.commands;
 import java.util.function.Supplier;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.SwerveModuleState; // swerve 狀態
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.GamepadJoystick;
@@ -16,4 +17,74 @@ public class SwerveJoystickCmd extends CommandBase {
     private final Supplier<Boolean> fieldOrientedFunction;
     private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
 
+    public SwerveJoystickCmd(SwerveSubsystem swerveSubsystem,
+            Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> turningSpdFunction,
+            Supplier<Boolean> fieldOrientedFunction) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.xSpdFunction = xSpdFunction;
+        this.ySpdFunction = ySpdFunction;
+        this.turningSpdFunction = turningSpdFunction;
+        this.fieldOrientedFunction = fieldOrientedFunction;
+        this.xLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.yLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.turningLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        // 1. Get real-time joystick inputs
+        double xSpeed = xSpdFunction.get();
+        double ySpeed = ySpdFunction.get();
+        double turningSpeed = turningSpdFunction.get();
+
+        // 2. Apply deadband
+        xSpeed = Math.abs(xSpeed) > GamepadJoystick.kDeadband ? xSpeed : 0.0;
+        ySpeed = Math.abs(ySpeed) > GamepadJoystick.kDeadband ? ySpeed : 0.0;
+        turningSpeed = Math.abs(turningSpeed) > GamepadJoystick.kDeadband ? turningSpeed : 0.0;
+
+        //if (xSpeed == 0 && ySpeed == 0 && turningSpeed == 0) {
+        //    this.swerveSubsystem.resetTurningmotor();
+        //    return;
+        //}
+
+        // 3. Make the driving smoother
+        xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        ySpeed = yLimiter.calculate(ySpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+        turningSpeed = turningLimiter.calculate(turningSpeed)
+                * DriveConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond;
+
+        // 4. Construct desired chassis speeds
+        ChassisSpeeds chassisSpeeds;
+        if (fieldOrientedFunction.get()) {
+            // Relative to field
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                    xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
+            SmartDashboard.putString("chassis moving type", "field");
+        } else {
+            // Relative to robot
+            chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+            SmartDashboard.putString("chassis moving type", "robot");
+        }
+
+        // 5. Convert chassis speeds to individual module states
+        SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+
+        // 6. Output each module states to wheels
+        swerveSubsystem.setModuleStates(moduleStates);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveSubsystem.stopModules();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
