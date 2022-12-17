8f95e3b6de9976770a50aff1323860b00294c7d1
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2022-12-18 00:03:54 +0800

    87 行註解調整

--- Teamwork_1/src/main/java/frc/robot/Constants.java
+++ Teamwork_1/src/main/java/frc/robot/Constants.java
@@ -84,7 +84,7 @@ public final class Constants {
         public static final double kElevatorMotorWheelDiameterMeters = Units.inchesToMeters(kElevatorMotorWheelRadius * 2); // 馬達輪 inch -> m
         public static final double kElevatorUpperWheelDiameterMeters = Units.inchesToMeters(kElevatorUpperWheelRadius * 2); // 升降輪 inch -> m
         public static final double kElevatorMotorGearRatio = 1 / 8.14; // 齒輪轉動比
-        public static final double kElevatorEncoderRot2Meter = kElevatorMotorGearRatio * Math.PI * kElevatorMotorWheelDiameterMeters; // 以 Encorder 的值轉換為升降之位置，單位為 m
+        public static final double kElevatorEncoderRot2Meter = kElevatorMotorGearRatio * Math.PI * kElevatorMotorWheelDiameterMeters; // 以 Encorder 的值轉換為現在高度，單位為 m
         public static final double kElevatorEncoderRPM2MeterPerSec = kElevatorEncoderRot2Meter / 60; // 將單位轉換為 m/s
     }
 
