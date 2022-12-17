58638d6987af8fd6f46bf1b65a8f76e1ef5a6791
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2022-12-18 00:36:44 +0800

    完成 Elevator 子程式

--- Teamwork_1/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ Teamwork_1/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,8 +1,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SPI; // 串和外設介面 (同步串行通信)
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; // 計分版 (即時顯示)
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; // 計分版 (即時顯示)
 import edu.wpi.first.wpilibj2.command.SubsystemBase; // 寫 Subsystems 需引入的模板
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.RobotMap.ElevatorPort;
@@ -10,6 +9,21 @@ import frc.robot.RobotMap.ElevatorPort;
 
 public class ElevatorSubsystem extends SubsystemBase {
     private final ElevatorModule LeftMotor = new ElevatorModule(ElevatorPort.kLeftMotorPort, false); // 設定左馬達模組
-    private final ElevatorModule RightMotor = new ElevatorModule(ElevatorPort.kRightMotorPort, false); // 設定右馬達模組
+    private final ElevatorModule RightMotor = new ElevatorModule(ElevatorPort.kRightMotorPort, true); // 設定右馬達模組
 
+
+    public ElevatorSubsystem() { // 建構函式
+        new Thread(() -> {}).start();
+    }
+
+    @Override
+    public void periodic() {
+        LeftMotor.putDashboard(); // 上傳左電梯高度 Encoder
+        RightMotor.putDashboard(); // 上傳右電梯高度 Encoder
+    }
+
+    public void stopModules() { // 停止所有模組
+        LeftMotor.stop();
+        RightMotor.stop();
+    }
 }
