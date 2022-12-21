b8d290a4af25a2176c3e8730736899677cdb5dc7
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2022-12-21 13:20:59 +0800

    創建 Fixture 控制程式檔

---
+++ Teamwork_1/src/main/java/frc/robot/subsystems/FixtureModule.java
@@ -0,0 +1,14 @@
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax; // Can for spark
+import com.revrobotics.RelativeEncoder; // 相對編碼器
+import com.revrobotics.CANSparkMaxLowLevel.MotorType; // 馬達類型
+import com.revrobotics.CANSparkMax.IdleMode; // 引入閒置函式庫
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; // 記分板(即時顯示數值)
+// import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.ElevatorModuleConstants;
+
+public class FixtureModule {
+    
+}
---
+++ Teamwork_1/src/main/java/frc/robot/subsystems/FixtureSubsystem.java
@@ -0,0 +1,13 @@
+
+package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; // 計分版 (即時顯示)
+import edu.wpi.first.wpilibj2.command.SubsystemBase; // 寫 Subsystems 需引入的模板
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.RobotMap.ElevatorPort;
+
+public class FixtureSubsystem extends SubsystemBase {
+
+    
+
+}
